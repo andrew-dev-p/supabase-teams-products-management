a7d4e1e1b35aa94d5e48e8eb6a0877596b36c4fa
andrew-dev-p <popov.andrew.dev@gmail.com>
2025-05-05 13:35:01 +0300

    Set up basic color palette

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+
 export default function Home() {
-  return <div className="text-3xl font-bold underline">Hello, world!</div>;
+  return (
+    <div className="flex flex-col gap-4 w-[200px] mx-auto mt-5">
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button size="sm">Small</Button>
+      <Button size="lg">Large</Button>
+      <Button size="icon">
+        <Plus />
+      </Button>
+    </div>
+  );
 }
